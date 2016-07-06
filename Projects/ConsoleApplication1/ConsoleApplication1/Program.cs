ec0456c9c2dc9f8494749e62dbd5f53e399b2e2f
ChanachaiJN <chanachai.jn@gmail.com>
2016-07-06 13:32:14 +0700

    first work

---
+++ Projects/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string random = " I love Devascloud";
+            string[] names = new string[5] { "black", "blue", "red", "white", "yellow" };
+            foreach (char e in String.Join(" ", names))
+            {
+
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
